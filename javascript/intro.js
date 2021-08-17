b10166ec5bdedf5e2abac7a492c37f063c744f86
Rohan Mathew Alex <52772228+rohanmathewalex@users.noreply.github.com>
2021-08-17 09:04:43 +0530

    javascript roadmap stared

---
+++ javascript/intro.js
@@ -0,0 +1,6 @@
+/*
+- what is javascript
+   JAVASCRIPT is a high level object-oriented ,multi-paradigm programming language.
+- The  role of javascript in web development
+   HTML,CSS,javascript these 3 technology all work together to create beautiful interactive and dyanamic website or web applications.  
+ */
